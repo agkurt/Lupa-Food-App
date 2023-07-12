9a8be2029fed961bb626bff6c3875ce2ffd1237f
Ahmet Göktürk Kurt <ahmetgokturkkurt3@gmail.com>
2023-07-13 00:15:18 +0300

    Initial Commit

--- Lupa/Controllers/Home/HomeViewController.swift
+++ Lupa/Controllers/Home/HomeViewController.swift
@@ -24,6 +24,11 @@ class HomeViewController: UIViewController {
         .init(id: "id1", name: "American Food5", image: "https://picsum.photos/100/200")
         
     ]
+    
+    var populars : [Dish] = [
+        .init(id: "id1", name: "garry", description: "this is the best food at the moment", image: "https://picsum.photos/100/200", calories: 10.20),
+        .init(id: "id2", name: "waffle", description: "this is the best food at the moment", image: "https://picsum.photos/100/200", calories: 10.20)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +38,7 @@ class HomeViewController: UIViewController {
     
     private func registerCells() {
         CategoryCollectionView.register(UINib(nibName: CategoryCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
-        popularCollectionView.register(UINib(nibName: CategoryCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
+        popularCollectionView.register(UINib(nibName: DishPotraitCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: DishPotraitCollectionViewCell.identifier)
     }
 
 }
@@ -41,7 +46,15 @@ class HomeViewController: UIViewController {
 extension HomeViewController:UICollectionViewDelegate , UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categories.count
+        switch collectionView {
+        case CategoryCollectionView:
+            return categories.count
+            
+        case popularCollectionView:
+            return populars.count
+            
+        default:return 0
+        }
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.identifier, for: indexPath) as! CategoryCollectionViewCell
--- Lupa/CustomViews/CollectionViewCells/DishPotraitCollectionViewCell/DishPotraitCollectionViewCell.swift
+++ Lupa/CustomViews/CollectionViewCells/DishPotraitCollectionViewCell/DishPotraitCollectionViewCell.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class DishPotraitCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = String("DishPotraitCollectionViewCell")
+    
     @IBOutlet weak var dishPotraitLbl: UILabel!
     @IBOutlet weak var dishPotraitImageView: UIImageView!
     @IBOutlet weak var caloriesLabel: UILabel!
     @IBOutlet weak var descriptionLbl: UILabel!
     
     func setup(dish : Dish) {
-        
+        dishPotraitLbl.text = dish.name
+        dishPotraitImageView.kf.setImage(with: dish.image?.asURL)
+        caloriesLabel.text = dish.calories?.formatted()
+        descriptionLbl.text = dish.description
     }
     
 }
